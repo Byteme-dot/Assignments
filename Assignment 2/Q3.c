d7ae0ba67a1ab5f22817befba2d4ad0ae17926fa
Byteme-dot <124488563+Byteme-dot@users.noreply.github.com>
2024-11-29 10:38:12 +0530

    new Questions

---
+++ Assignment 2/Q3.c
@@ -0,0 +1,16 @@
+//WAP to find who scored first “99” in an array marks. 
+#include<stdio.h>
+int main(){
+    int num_std;
+    printf("Enter No of student's data you want to enter: ");
+    scanf("%d",&num_std);
+    int arr_marks[num_std];
+    for(int i=0;i<num_std;i++){
+        printf("Entet Student %d's Marks: ");
+        scanf("%d",&arr_marks[i]);
+    }
+    return 0;
+}
+void find_99(int num_std,int arr_std[num_std]){
+    for (int i=0;i<num_std;i==)
+}
